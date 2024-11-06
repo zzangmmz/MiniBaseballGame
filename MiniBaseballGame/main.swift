d6aadb547d22efbdadce3c84f0c26eca0cc20c2b
zzangmmz <leemj036@gmail.com>
2024-11-06 18:34:22 +0900

    feat: Level1 요구사항 구현

--- MiniBaseballGame/main.swift
+++ MiniBaseballGame/main.swift
@@ -1,6 +1,9 @@
 // Level1
 import Foundation
 
+var game = MiniBaseballGame()
+game.play()
+
 struct MiniBaseballGame {
     private var inputNumbers = [Int]()
     private var answerNumbers = [Int]()
@@ -9,7 +12,14 @@ struct MiniBaseballGame {
         return [Int.random(in: 1...9), Int.random(in: 1...9), Int.random(in: 1...9)]
     }
     
-    func play() {
+    mutating func play() {
+        print("1에서 9까지의 서로 다른 임의의 수 3개를 입력하세요.")
+        inputNumbers = readLine()!.split(separator: " ").map { (Int(String($0))!) }
+        answerNumbers = makeAnswerNumbers()
         
+        print(inputNumbers.contains(answerNumbers) ? "\n정답입니다!" : "\n오답입니다...")
+        // 정답 확인용
+        print("입력: \(inputNumbers)")
+        print("정답: \(answerNumbers)")
     }
 }
