db205a27895a605a14453ebbf4ae0802df915b77
zzangmmz <leemj036@gmail.com>
2024-11-07 14:50:22 +0900

    refactor: 안내 메세지, 오류 메세지 출력 Enum 프로토콜 변경(String ->  CustomStringConvertible)

--- MiniBaseballGame/ErrorDescription.swift
+++ MiniBaseballGame/ErrorDescription.swift
@@ -1,9 +1,13 @@
-enum ErrorDescription: String {
-    case wrongChoice = "\n올바른 숫자를 입력해주세요!"
-    case emptyLog = "\n게임 기록이 없습니다.\n"
-    case wrongInput = "올바르지 않은 입력값입니다\n"
+enum ErrorDescription: CustomStringConvertible {
+    case wrongChoice
+    case emptyLog
+    case wrongInput
     
-    func printError() {
-        print(self.rawValue)
+    var description: String {
+        switch self {
+        case .wrongChoice: return "\n올바른 숫자를 입력해주세요!"
+        case .emptyLog: return "\n게임 기록이 없습니다.\n"
+        case .wrongInput: return "올바르지 않은 입력값입니다\n"
+        }
     }
 }
--- MiniBaseballGame/GuideDescription.swift
+++ MiniBaseballGame/GuideDescription.swift
@@ -1,11 +1,17 @@
-enum GuideDescription: String {
-    case greeting = "환영합니다! 원하시는 번호를 입력해주세요\n1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기"
-    case start = "\n< 게임을 시작합니다 >"
-    case input = "숫자를 입력하세요"
-    case correct = "정답입니다!\n"
-    case end = "\n< 숫자 야구 게임을 종료합니다 >"
+enum GuideDescription: CustomStringConvertible {
+    case greeting
+    case start
+    case enterInput
+    case correct
+    case end
     
-    func printGuide() {
-        print(self.rawValue)
+    var description: String {
+        switch self {
+        case .greeting: return "환영합니다! 원하시는 번호를 입력해주세요\n1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기"
+        case .start: return "\n< 게임을 시작합니다 >"
+        case .enterInput: return "숫자를 입력하세요"
+        case .correct: return "정답입니다!\n"
+        case .end: return "\n< 숫자 야구 게임을 종료합니다 >"
+        }
     }
 }
