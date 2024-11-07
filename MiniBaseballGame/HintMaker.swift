a7f162b0e0e46e3b54372deaccf3f42f65fdf31c
zzangmmz <leemj036@gmail.com>
2024-11-07 14:48:58 +0900

    refactor: BallAndStrike() -> HintMaker() 이름 변경, 생성자 수정

--- MiniBaseballGame/HintMaker.swift
+++ MiniBaseballGame/HintMaker.swift
@@ -1,21 +1,21 @@
-final class BallAndStrike {
+final class HintMaker {
     private var balls: Int
     private var strikes: Int
     
-    init() {
+    init(_ inputNumber: [Character], _ answerNumber: [Character]) {
         self.balls = 0
         self.strikes = 0
-    }
-    
-    func printHints(_ inputNumber: [Character], _ answerNumber: [Character]) {
+        
         for i in 0 ..< answerNumber.count {
             if inputNumber[i] == answerNumber[i] {
-                strikes += 1
+                self.strikes += 1
             } else if answerNumber.contains(inputNumber[i]) {
-                balls += 1
+                self.balls += 1
             }
         }
-        
+    }
+    
+    func printHints() {
         if strikes != 0 && balls != 0 {
             print("\(strikes)스트라이크 \(balls)볼\n")
         } else if strikes == 0 && balls != 0 {
